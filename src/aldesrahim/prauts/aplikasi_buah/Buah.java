a9481917d106fde47b663802e24177315da5f206
Al Desrahim <37100012+aldesrahim@users.noreply.github.com>
2022-10-28 00:13:19 +0700

    Create Aplikasi Buah

---
+++ src/aldesrahim/prauts/aplikasi_buah/Buah.java
@@ -0,0 +1,88 @@
+package aldesrahim.prauts.aplikasi_buah;
+
+import aldesrahim.prauts.Helper;
+
+import java.util.List;
+
+public class Buah {
+    String nama;
+    String notelp;
+
+    public Buah(String nama, String notelp) {
+        this.nama = nama;
+        this.notelp = notelp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("===== SELAMAT DATANG DI TOKO BUAH HOTARU =====");
+        System.out.println(Helper.strRepeat("-", 46));
+
+        String nama = Helper.ask("Masukkan nama pelanggan: ");
+        String notelp = Helper.ask("Masukkan no telepon: ");
+
+        Buah buah = new Buah(nama, notelp);
+        buah.menu();
+    }
+
+    public void menu() {
+        String input;
+
+        System.out.println("Jenis buah yang ingin dibeli:");
+        System.out.println("1. Jeruk");
+        System.out.println("2. Mangga");
+
+        input = Helper.ask("Pilihan: ");
+
+        FruitInterface fruitObj = switch (input) {
+            case "1" -> new Jeruk();
+            case "2" -> new Mangga();
+            default -> null;
+        };
+
+        if (fruitObj == null) {
+            this.menu();
+            return;
+        }
+
+        fruitObj.menu();
+        Double total = fruitObj.checkout();
+
+        if (total == null) {
+            System.out.println("Tidak ada buah dalam keranjang");
+            return;
+        }
+
+        System.out.println(Helper.strRepeat("-", 35));
+        System.out.println("----------Struk Pembelian----------");
+        System.out.println("\t\t Yang dibeli");
+        System.out.println("\t Buah\t\t\t\t Banyak Buah");
+
+        int i = 1;
+        for (DTOInterface cart: fruitObj.getCarts()) {
+            System.out.printf("%-3s %-20s %s kg\n", (i++) + ".", cart.getNama(), cart.getQty());
+        }
+
+        System.out.println("Total yang harus dibayar: Rp " + Helper.numberFormat(total));
+        System.out.println(Helper.strRepeat("-", 35));
+        System.out.println("Terima kasih telah berbelanja " + this.nama);
+        System.out.println("\t Jangan lupa datang kembali");
+    }
+
+    public interface DTOInterface {
+        String getNama();
+
+        Double getHarga();
+
+        Integer getQty();
+
+        void setQty(Integer qty);
+    }
+
+    public interface FruitInterface {
+        void menu();
+
+        Double checkout();
+
+        List<Buah.DTOInterface> getCarts();
+    }
+}
---
+++ src/aldesrahim/prauts/aplikasi_buah/Jeruk.java
@@ -0,0 +1,116 @@
+package aldesrahim.prauts.aplikasi_buah;
+
+import aldesrahim.prauts.Helper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Jeruk implements Buah.FruitInterface {
+    protected List<Buah.DTOInterface> fruits = new ArrayList<>();
+    protected List<Buah.DTOInterface> carts = new ArrayList<>();
+
+    public Jeruk() {
+        this.init();
+    }
+
+    private void init() {
+        class BuahDTO implements Buah.DTOInterface {
+            final String nama;
+            final Double harga;
+            Integer qty = 0;
+
+            public BuahDTO(String nama, Object harga) {
+                this.nama = nama;
+                this.harga = Double.parseDouble(harga.toString());
+            }
+
+            public String getNama() {
+                return this.nama;
+            }
+
+            public Double getHarga() {
+                return this.harga;
+            }
+
+            public Integer getQty() {
+                return this.qty;
+            }
+
+            public void setQty(Integer qty) {
+                this.qty = qty;
+            }
+
+        }
+
+        Buah.DTOInterface fruit1 = new BuahDTO("Jeruk Nipis", 5000);
+        Buah.DTOInterface fruit2 = new BuahDTO("Jeruk Manis", 10000.00);
+        Buah.DTOInterface fruit3 = new BuahDTO("Jeruk Bali", 50000.00);
+
+        this.fruits.addAll(Arrays.asList(fruit1, fruit2, fruit3));
+    }
+
+    public List<Buah.DTOInterface> getCarts() {
+        return this.carts;
+    }
+
+    public void menu() {
+        System.out.println("\t Jenis buah jeruk");
+        System.out.println(Helper.strRepeat("-", 30));
+
+        int i = 1;
+
+        for (Buah.DTOInterface fruit : this.fruits) {
+            System.out.printf("%-3s %-20s Rp %s\n", (i++) + ".", fruit.getNama(), Helper.numberFormat(fruit.getHarga()));
+        }
+        System.out.printf("%-3s %-20s\n", "9.", "Check out");
+
+        while (true) {
+            Integer input = Helper.askInt("Pilihan anda: ");
+
+            if (input.equals(9)) {
+                break;
+            }
+
+            if (input.compareTo(this.fruits.size()) > 0) {
+                continue;
+            }
+
+            Integer qty = Helper.askInt("Banyaknya buah yang dibeli: ");
+
+            if (this.addToChart(input - 1, qty).equals(false)) {
+                System.out.println("Gagal ditambahkan");
+                continue;
+            }
+
+            System.out.println("Berhasil ditambahkan");
+        }
+    }
+
+    private Boolean addToChart(Integer i, Integer qty) {
+        try {
+            Buah.DTOInterface fruit = this.fruits.get(i);
+            fruit.setQty(qty);
+
+            this.carts.add(fruit);
+        } catch (IndexOutOfBoundsException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Double checkout() {
+        if (this.carts.size() == 0) {
+            return null;
+        }
+
+        double total = 0.0;
+
+        for (Buah.DTOInterface cart : this.carts) {
+            total += cart.getHarga() * cart.getQty();
+        }
+
+        return total;
+    }
+}
